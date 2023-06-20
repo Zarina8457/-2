085f7016b2ea44517d799d022a9c336d95c6596d
@Zarina8457 <lu_angel@list.ru>
2023-06-20 07:57:52 +0300

    Решила Zadachu_№4

--- Urok1/Zadacha_№4/Program.cs
+++ Urok1/Zadacha_№4/Program.cs
@@ -4,4 +4,22 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine();
+Console.WriteLine("Задача № 4. Максимальное из трёх чисел");
 
+Console.Write("Введи первое число: ");
+int firstNumberOfThree = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи второе число: ");
+int secondNumberOfThree = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи третье число: ");
+int thirdNumberOfThree = Convert.ToInt32(Console.ReadLine());
+
+int maxNumber = firstNumberOfThree;   // дополнительная переменная
+
+if (maxNumber < secondNumberOfThree) {
+  maxNumber = secondNumberOfThree;
+}
+if (maxNumber < thirdNumberOfThree) {
+  maxNumber = thirdNumberOfThree;
+}
+Console.WriteLine("MAX= " + maxNumber);
