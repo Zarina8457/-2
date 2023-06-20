12c13e5088834287f526e455708877c68ac1df65
@Zarina8457 <lu_angel@list.ru>
2023-06-20 06:35:38 +0300

    Решила Zadachu_№10

--- Urok2/Zadacha_№10/Program.cs
+++ Urok2/Zadacha_№10/Program.cs
@@ -4,3 +4,9 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.Clear();
+Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
