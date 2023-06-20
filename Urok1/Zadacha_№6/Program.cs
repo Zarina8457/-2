1fc78506d783ab32c485170321281d0837a717e7
@Zarina8457 <lu_angel@list.ru>
2023-06-20 20:33:44 +0300

    Решила Zadachu_№6

--- Urok1/Zadacha_№6/Program.cs
+++ Urok1/Zadacha_№6/Program.cs
@@ -5,7 +5,15 @@
 // -3 -> нет
 // 7 -> нет
 
+Console.WriteLine();
+Console.WriteLine("Задача № 6. Чётное или нет?");
 
+Console.Write("Введи тестируемое число: ");
+int testNumber = Convert.ToInt32(Console.ReadLine());
 
-
- 
+if (testNumber % 2 == 0) { 
+    Console.WriteLine("Да - чётное");
+     } 
+else  { 
+    Console.WriteLine("Нет - нечётное"); 
+     } 
