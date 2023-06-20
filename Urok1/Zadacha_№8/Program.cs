bf27fa57e5372115f8b1d3f488e539c8030d193b
@Zarina8457 <lu_angel@list.ru>
2023-06-20 08:44:50 +0300

    Решила Zadachu_№8

--- Urok1/Zadacha_№8/Program.cs
+++ Urok1/Zadacha_№8/Program.cs
@@ -2,3 +2,22 @@
 //а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine();
+Console.WriteLine("Задача № 8. Чётные от 1 до N");
+
+Console.Write("Введи число N: ");
+int theNumberN = Convert.ToInt32(Console.ReadLine());
+
+int counter = 1;
+
+while (counter <= theNumberN)
+{
+  if (counter % 2 != 1) {
+    Console.Write(counter);
+    if (counter < (theNumberN - 1)) {     // расстановка запятых
+      Console.Write(", ");        
+    }
+  }
+  counter += 1;
+}
