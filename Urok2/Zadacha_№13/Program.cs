fee38973de66df39d54e29cf2cd8d0c4b390505f
@Zarina8457 <lu_angel@list.ru>
2023-06-20 06:44:16 +0300

    Решила Zadachu_№13

--- Urok2/Zadacha_№13/Program.cs
+++ Urok2/Zadacha_№13/Program.cs
@@ -3,3 +3,15 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.WriteLine("Задача 13 ");
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
