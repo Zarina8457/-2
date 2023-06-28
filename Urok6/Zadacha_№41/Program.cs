e94bb5810ece4f3ea92257b2694f06cd481c93d1
@Zarina8457 <lu_angel@list.ru>
2023-06-28 16:25:16 +0300

    Решила Zadachu_№41

--- Urok6/Zadacha_№41/Program.cs
+++ Urok6/Zadacha_№41/Program.cs
@@ -1,5 +1,22 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+// 1, -7, 567, 89, 223 -> 3 /*ошибка 4 числа больше 0*
 
+Console.WriteLine($"\nЗадача 41. Cколько чисел больше 0 ввёл пользователь?");
 
+int Recursion(int m, int count)
+{
+    if (m > 0)
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0)
+        {
+            count = count + 1;
+            return Recursion(m - 1, count);
+        }
+        else return Recursion(m - 1, count);
+    }
+    else return count;
+}
+Console.WriteLine($"Пользователь ввёл {Recursion(5, 0)} чисел больше ноля");
