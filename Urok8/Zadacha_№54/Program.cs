e06f8b140fa8c15e32926c1e16659b1a720a633f
@Zarina8457 <lu_angel@list.ru>
2023-07-05 01:08:22 +0300

    Ришила Zadachu_№54

--- Urok8/Zadacha_№54/Program.cs
+++ Urok8/Zadacha_№54/Program.cs
@@ -9,3 +9,57 @@
 // 9 5 3 2
 // 8 4 4 2
 
+Console.WriteLine($"\nЗадача 54.");
+
+int[,] table = new int[3, 4];
+FillArrayRandom(table);
+PrintArray(table);
+SortToLower(table);
+Console.WriteLine();
+PrintArray(table);
+
+
+
+void FillArrayRandom(int[,] array)   // Функция заполнения массива рандомно числами от 1 до 9
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void SortToLower(int[,] array)  // Функция сортировки элементов в строке двумерного массива, по убыванию
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Функция вывода двумерного массива
+void PrintArray(int[,] array)   
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
