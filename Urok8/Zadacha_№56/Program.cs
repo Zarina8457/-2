ed772c102e96ed779fecf204abd4c3fdf51ec6f9
@Zarina8457 <lu_angel@list.ru>
2023-07-05 01:24:37 +0300

    Решила Zadachu _№56

--- Urok8/Zadacha_№56/Program.cs
+++ Urok8/Zadacha_№56/Program.cs
@@ -7,3 +7,56 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+Console.WriteLine($"\nЗадача 56.");
+
+int[,] table = new int[4, 4];
+FillArrayRandom(table);
+PrintArray(table);
+Console.WriteLine();
+NumberRowMinSumElements(table);
+
+
+void NumberRowMinSumElements(int[,] array)  // Функция вывода номера строки (не индекса) с наименьшей суммой элементов.
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < table.GetLength(1); i++)
+    {
+        minRow += table[0, i];
+    }
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++) sumRow += table[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+void PrintArray(int[,] array)  // Функция вывода двумерного массива.
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrayRandom(int[,] array)  // Функция заполнения массива рандомно числами от 1 до 9.
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
