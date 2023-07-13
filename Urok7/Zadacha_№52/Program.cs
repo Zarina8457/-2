0132e4030ffbff09e255cdad752671da82fc265f
@Zarina8457 <lu_angel@list.ru>
2023-07-14 01:44:20 +0300

    Добавила условия к Zadacham_№64 №66 №68

--- Urok7/Zadacha_№52/Program.cs
+++ Urok7/Zadacha_№52/Program.cs
@@ -7,70 +7,75 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
-Console.WriteLine($"\nЗадача 52.");
-
-// int rows = ReadInt("Введите номер строки: ");
-// int colums = ReadInt("Введите номер столбца: ");
-
-int rows = 3;
-int colums = 4;
-int[,] numbers = new int[rows, colums];
-FillArray2D(numbers);
-PrintArray2D(numbers);
-
-double[] avgNumbers = new double[numbers.GetLength(1)];
-
-for (int i = 0; i < numbers.GetLength(1); i++)
+internal class Program
 {
-    double result = 0.0;
-    for (int j = 0; j < numbers.GetLength(0); j++)
+    private static void Main(string[] args)
     {
-        result += numbers[j, i];
-    }
-    avgNumbers[i] = result / numbers.GetLength(0);
-}
-PrintArray(avgNumbers);
+        Console.WriteLine($"\nЗадача 52.");
 
+        // int rows = ReadInt("Введите номер строки: ");
+        // int colums = ReadInt("Введите номер столбца: ");
 
-// Заполнение массива рандомными вещественными числами
-void FillArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
+        int rows = 3;
+        int colums = 4;
+        int[,] numbers = new int[rows, colums];
+        FillArray2D(numbers);
+        PrintArray2D(numbers);
+
+        double[] avgNumbers = new double[numbers.GetLength(1)];
+
+        for (int i = 0; i < numbers.GetLength(1); i++)
         {
-            array[i, j] = new Random().Next(1, 10);
+            double result = 0.0;
+            for (int j = 0; j < numbers.GetLength(0); j++)
+            {
+                result += numbers[j, i];
+            }
+            avgNumbers[i] = result / numbers.GetLength(0);
+        }
+        PrintArray(avgNumbers);
+
+
+        // Заполнение массива рандомными вещественными числами
+        void FillArray2D(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(1, 10);
+                }
+            }
+        }
+
+        //  Функция вывода двумерного массива в терминал
+        void PrintArray2D(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        // Функция вывода массива в терминал 
+        void PrintArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
-}
 
-//  Функция вывода двумерного массива в терминал
-void PrintArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
+    private static int ReadInt(string message)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
+        Console.Write(message);
+        return Convert.ToInt32(Console.ReadLine());
     }
-    Console.WriteLine();
-}
-
-// Функция вывода массива в терминал 
-void PrintArray(double[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
-
-// Функция ввода 
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
 }
