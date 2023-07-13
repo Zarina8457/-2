0f21be2af08372f716e1b90fdee0f25ecd63cc19
@Zarina8457 <lu_angel@list.ru>
2023-07-14 01:49:34 +0300

    Решила Zadachu_№64

--- Urok9/Zadacha _№64/Program.cs
+++ Urok9/Zadacha _№64/Program.cs
@@ -3,3 +3,23 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.WriteLine($"\nЗадача 64.");
+
+int n = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+    if (count > n) return;
+    PrintNumber(n, count + 1);
+    Console.Write(count + ", ");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
